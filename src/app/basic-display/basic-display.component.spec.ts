7ddbbed8ee8f1df5f1f42167c7a5817bada2f000
Kevin Wright <kevinwright@Kevins-MBP.grandenetworks.net>
2021-03-18 21:08:59 -0500

    initial commit

---
+++ src/app/basic-display/basic-display.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BasicDisplayComponent } from './basic-display.component';
+
+describe('BasicDisplayComponent', () => {
+  let component: BasicDisplayComponent;
+  let fixture: ComponentFixture<BasicDisplayComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BasicDisplayComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BasicDisplayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
